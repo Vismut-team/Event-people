17160c6ae43861896f54a5c20104ba70a3949bea
Maksim_Tataurov <maksimuspiter@gmail.com>
2023-07-20 17:41:57 +0300

    add watcher (auth user push to home)

--- project/frontend/src/views/AuthView.vue
+++ project/frontend/src/views/AuthView.vue
@@ -1,12 +1,23 @@
 <script setup>
-import { ref } from "vue"
+import { ref, watch, computed } from "vue"
+import router from '../router'
+
 import LoginWindow from '../components/LoginWindow.vue'
 import RegistrationWindow from '../components/RegistrationWindow.vue'
+import { useApiStore } from '@/stores/ApiStore'
 
+const api = useApiStore()
 const login_active = ref(true)
 const change_window = () => {
     login_active.value = !login_active.value
 }
+const auth = computed(() => { return api.userAuth })
+watch(auth, () => {
+    if (auth.value === true) {
+        router.push({ name: 'home' })
+    }
+})
+
 </script>
 <template>
     <div class="mt-3 mb-5 d-flex justify-content-center">
