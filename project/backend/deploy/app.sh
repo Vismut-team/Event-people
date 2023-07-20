b06f580289d109b8adec4551e5709b7b6635ea00
Maksim_Tataurov <maksimuspiter@gmail.com>
2023-07-20 13:50:06 +0300

    ref host

--- project/backend/deploy/app.sh
+++ project/backend/deploy/app.sh
@@ -2,4 +2,4 @@
 
 alembic upgrade head
 
-gunicorn app.main:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind=0:8000
+gunicorn app.main:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
